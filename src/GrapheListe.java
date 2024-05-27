2ef31bdfb244e383854f3b85f2725ac6ed2f8cf7
Antonin-M <146951595+Antonin-M@users.noreply.github.com>
2024-05-27 17:09:16 +0200

    Squelette GrapheListe

---
+++ src/GrapheListe.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrapheListe implements Graphe {
+    private ArrayList<String> noeuds;
+    private ArrayList<Arcs> adjacence;
+    @Override
+    public List<String> listeNoeuds() {
+        return null;
+    }
+
+    @Override
+    public List<Arc> suivants(String n) {
+        return null;
+    }
+
+    public void ajouterArc(String depart, String destination, double cout) {
+
+    }
+
+    public int getIndice(String n) {
+        return 0;
+    }
+}
