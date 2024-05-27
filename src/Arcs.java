835ae92f907681e421391f78da04f0248ba6a261
Antonin-M <146951595+Antonin-M@users.noreply.github.com>
2024-05-27 16:44:14 +0200

    Classe arcs

--- src/Arcs.java
+++ src/Arcs.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arcs {
+    private List<Arc> arcs;
+
+    public Arcs() {
+        this.arcs = new ArrayList<Arc>();
+    }
+
+    public void ajouterArc (Arc a) {
+        this.arcs.add(a);
+    }
+
+    public List<Arc> getArcs() {
+        return arcs;
+    }
 }
