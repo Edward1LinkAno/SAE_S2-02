358809796eb77041be87b74cc85a963d56eb08bc
Edward1LinkAno <edward.linkano.gaming@gmail.com>
2024-05-27 17:34:13 +0200

    Update MainGraphe.java

--- src/Main/MainGraphe.java
+++ src/Main/MainGraphe.java
@@ -2,6 +2,20 @@ package Main;
 
 import Graphes.* ;
 
+import java.util.ArrayList;
+
 public class MainGraphe {
 
+    public static void main(String[] args) {
+        ArrayList<String> noeuds = new ArrayList<>() ;
+        noeuds.add("A") ;
+        noeuds.add("B") ;
+        noeuds.add("C") ;
+        noeuds.add("D") ;
+        noeuds.add("E") ;
+
+        Graphe graphe = new GrapheListe(noeuds) ;
+
+        graphe.ajouterArc() ;
+    }
 }
