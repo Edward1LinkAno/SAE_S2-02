ba265627f09da181cce58313372234602958e851
Antonin-M <146951595+Antonin-M@users.noreply.github.com>
2024-05-27 16:59:41 +0200

    Ajout des commentaires et de la javadoc

--- src/Arc.java
+++ src/Arc.java
@@ -1,6 +1,8 @@
+
+/* Un objet de la classe Arc représente un arc partant d’un noeud. */
 public class Arc {
-    private String dest;
-    private double cout;
+    private String dest; /* représente le nom du nœud destination de l’arc */
+    private double cout; /* correspondant au coût (ou poids) de l’arc */
 
     public Arc(String dest, double cout) {
         this.dest = dest;
--- src/Arcs.java
+++ src/Arcs.java
@@ -1,17 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/* Classe permettant de gérer une liste d'Arc */
 public class Arcs {
-    private List<Arc> arcs;
+    private List<Arc> arcs; /* Représente la liste d'Arc a gérer */
 
+    /**
+     * Constructeur vide qui construit une liste vide d'Arc
+     */
     public Arcs() {
         this.arcs = new ArrayList<Arc>();
     }
 
+    /**
+     * Methode ajouterArc qui ajoute un arc à notre liste
+     * @param a l'arc a ajouter
+     */
     public void ajouterArc (Arc a) {
         this.arcs.add(a);
     }
 
+    /**
+     * Methode GetArcs qui nous retourne notre liste d'Arc
+     * @return l'attribut arcs
+     */
     public List<Arc> getArcs() {
         return arcs;
     }
