ef668b9b20e516218f966c4b2b1b13c54cdc6109
Antonin-M <146951595+Antonin-M@users.noreply.github.com>
2024-05-28 15:11:14 +0200

    début TestGrapheListe

--- test/TestGrapheListe.java
+++ test/TestGrapheListe.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGrapheListe {
     /**
-     * test de avec un répertoire valide
+     * test de avec un graphe construit correctement
      */
     @Test
     public void testValide() {
@@ -43,4 +43,43 @@ public class TestGrapheListe {
         assertEquals(5,noeuds.size());
         assertEquals(Gl.getAdjacence().get(1).getArcs().get(0).getDest(),"E");
     }
+
+    /**
+     * test de avec un graphe construit correctement
+     */
+    @Test
+    public void testArcInexistant() {
+        ArrayList<String> noeuds = new ArrayList<>() ;
+        noeuds.add("A") ;
+        noeuds.add("B") ;
+        noeuds.add("C") ;
+        noeuds.add("D") ;
+        noeuds.add("E") ;
+        GrapheListe Gl = new GrapheListe(noeuds);
+
+        Gl.ajouterArc("A","B",12) ;
+        Gl.ajouterArc("A","D",87) ;
+        Gl.ajouterArc("B","E",11) ;
+        Gl.ajouterArc("C","A",19) ;
+        Gl.ajouterArc("D","B",23) ;
+        Gl.ajouterArc("D","C",10) ;
+        Gl.ajouterArc("E","D",43) ;
+
+        System.out.println(Gl);
+
+        try {
+            System.out.println(Gl.suivants("B"));
+        } catch (NullPointerException e) {
+            assertTrue(false);
+        }
+
+        assertEquals(5,noeuds.size());
+        Exception IndexOutOfBoundsException = assertThrows(
+                java.lang.IndexOutOfBoundsException.class,
+                ()-> {
+                    Gl.getAdjacence().get(1).getArcs().get(123).getDest();}
+        );
+    }
+
+
 }
