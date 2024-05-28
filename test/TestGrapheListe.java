c8b77c97fe0e6a4853d9533cdabd950fbfa2bbf8
Antonin-M <146951595+Antonin-M@users.noreply.github.com>
2024-05-28 15:17:45 +0200

    Fin TestGrapheListe

--- test/TestGrapheListe.java
+++ test/TestGrapheListe.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGrapheListe {
     /**
-     * test de avec un graphe construit correctement
+     * test avec un graphe construit correctement
      */
     @Test
     public void testValide() {
@@ -35,11 +35,11 @@ public class TestGrapheListe {
         }
 
         assertEquals(5,noeuds.size());
-        assertEquals(Gl.getAdjacence().get(1).getArcs().get(0).getDest(),"E");
+        assertEquals(Gl.getAdjacence().get(Gl.getIndice("B")).getArcs().get(0).getDest(),"E");
     }
 
     /**
-     * test de avec un graphe construit correctement
+     * test avec la recherche d'un arc inexistant
      */
     @Test
     public void testArcInexistant() {
@@ -71,9 +71,66 @@ public class TestGrapheListe {
         Exception IndexOutOfBoundsException = assertThrows(
                 java.lang.IndexOutOfBoundsException.class,
                 ()-> {
-                    Gl.getAdjacence().get(1).getArcs().get(123).getDest();}
+                    Gl.getAdjacence().get(Gl.getIndice("B")).getArcs().get(123).getDest();}
         );
     }
 
+    /**
+     * test avec des sommets sans adjacents
+     */
+    @org.junit.Test
+    public void testValideAvecNoeudInexistant() {
+        ArrayList<String> noeuds = new ArrayList<>() ;
+        noeuds.add("A") ;
+        noeuds.add("B") ;
+        noeuds.add("C") ;
+        noeuds.add("D") ;
+        noeuds.add("E") ;
+        GrapheListe Gl = new GrapheListe(noeuds);
 
+        Gl.ajouterArc("A","B",12) ;
+        Gl.ajouterArc("A","D",87) ;
+        Gl.ajouterArc("E","D",43) ;
+
+        System.out.println(Gl);
+
+        try {
+            System.out.println(Gl.suivants("E"));
+        } catch (NullPointerException e) {
+            assertTrue(false);
+        }
+
+        assertEquals(5,noeuds.size());
+        assertEquals(Gl.getAdjacence().get(Gl.getIndice("E")).getArcs().get(0).getDest(),"D");
+    }
+
+    /**
+     * test de avec un graphe construit Avec la liste de noeud Incomplète
+     */
+    @org.junit.Test
+    public void testValideAvecListeNoeudIncomplete() {
+        ArrayList<String> noeuds = new ArrayList<>() ;
+        noeuds.add("A") ;
+        noeuds.add("B") ;
+        GrapheListe Gl = new GrapheListe(noeuds);
+
+        Gl.ajouterArc("A","B",12) ;
+        Gl.ajouterArc("A","D",87) ;
+        Gl.ajouterArc("B","E",11) ;
+        Gl.ajouterArc("C","A",19) ;
+        Gl.ajouterArc("D","B",23) ;
+        Gl.ajouterArc("D","C",10) ;
+        Gl.ajouterArc("E","D",43) ;
+
+        System.out.println(Gl);
+
+        try {
+            System.out.println(Gl.suivants("D"));
+        } catch (NullPointerException e) {
+            assertTrue(false);
+        }
+
+        assertEquals(5,noeuds.size());
+        assertEquals(Gl.getAdjacence().get(Gl.getIndice("D")).getArcs().get(1).getDest(),"C");
+    }
 }
