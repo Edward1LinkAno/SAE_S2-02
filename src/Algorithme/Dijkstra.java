f1a06c5c3deb2c465cc6298fb91b61801c7e81a2
Antonin-M <146951595+Antonin-M@users.noreply.github.com>
2024-05-30 09:32:10 +0200

    Algorithme de Dijkstra problème ligne 91

--- src/Algorithme/Dijkstra.java
+++ src/Algorithme/Dijkstra.java
@@ -9,54 +9,53 @@ import java.util.ArrayList;
 
 public class Dijkstra {
 
-        public Valeur resoudre(Graphe g, String depart) {
+//        public Valeur resoudre(Graphe g, String depart) {
 //    Entrées :
                 //GrapheListe G;      //G un graphe orienté avec une pondération positive des arcs (coût ou poids)
-                Valeur A = new Valeur();
-                //Arc A=new Arc(g.listeNoeuds().get(g.listeNoeuds().indexOf(depart)),0);   //A un sommet (départ) de G
-                Valeur v = new Valeur();
+ //               Valeur A = new Valeur();
+                //Arc A=new Arc(g.listeNoeuds().get(g.listeNoeuds().indexOf(depart)),0);   //A un sommet (départ) de G//               Valeur v = new Valeur();
 
 //    Début
-                ArrayList<String> Q = new ArrayList<String>();     // utilisation d’une liste de noeuds à traiter
-                for (String s : g.listeNoeuds()) {
-                        for (Arc a : g.suivants(s)) {
-                                v.setValeur(s,Double.MAX_VALUE);
-                                v.setParent("indefini",s);
-                                Q.add(s);
-                        }
-                }
+//                ArrayList<String> Q = new ArrayList<String>();     // utilisation d’une liste de noeuds à traiter
+//                for (String s : g.listeNoeuds()) {
+//                        for (Arc a : g.suivants(s)) {
+//                                v.setValeur(s,Double.MAX_VALUE);
+//                                v.setParent(s,"indefini");
+//                                Q.add(s);
+//                        }
+//                }
 //    Pour chaque sommet v de G faire
 //       v.valeur <- Infini
 //       v.parent <- Indéfini
 //       Q <- Q U {v} // ajouter le sommet v à la liste Q
 //    }
-                A.setValeur(depart,0);
+//                A.setValeur(depart,0);
 //    A.valeur <- 0
-                while (!Q.isEmpty()) {
+//                while (!Q.isEmpty()) {
 //    Tant que Q est un ensemble non vide faire
-                        Valeur u = v;
-                        int index=0;
-                        for(int j=0;j<Q.size();j++) {
-                                if(v.getValeur(Q.get(j))>g.suivants(Q.get(j)).get(j).getCout()) {
-                                        v.setValeur(Q.get(j),g.suivants(Q.get(j)).get(j).getCout());
-                                        index=j;
-                                }
-                        }
-                        Q.remove(Q.get(index));
+//                        Valeur u = A;
+//                        int index=0;
+//                        for(int j=0;j<Q.size();j++) {
+//                                if(v.getValeur(Q.get(j))>g.suivants(Q.get(j)).get(j).getCout()) {
+//                                        v.setValeur(Q.get(j),g.suivants(Q.get(j)).get(j).getCout());
+//                                        index=j;
+//                                }
+//                        }
+//                        Q.remove(Q.get(index));
 
 
 //          u <- un sommet de Q telle que u.valeur est minimal
         //enlever le sommet u de la liste Q
 //          Q <- Q \ {u}
-                        for (int k=0; k<Q.size();k++) {
-                                if (g.suivants(Q.get(k)).get(k).getDest().equals(Q.get(k))) {
-                                        double d=u.getValeur(Q.get(k))+g.suivants(Q.get(k)).get(k).getCout();
-                                        if (d<v.getValeur(Q.get(k))) {
-                                                v.setValeur(Q.get(k),d);
-                                                v.setParent(Q.get(k),u.getParent(Q.get(k)));
-                                        }
-                                }
-                        }
+//                        for (int k=0; k<Q.size();k++) {
+//                                if (g.suivants(Q.get(k)).get(k).getDest().equals(Q.get(k))) {
+//                                        double d=u.getValeur(Q.get(k))+g.suivants(Q.get(k)).get(k).getCout();
+//                                        if (d<v.getValeur(Q.get(k))) {
+//                                                v.setValeur(Q.get(k),d);
+//                                                v.setParent(Q.get(k),u.getParent(Q.get(k)));
+//                                        }
+//                                }
+//                        }
 //          Pour chaque sommet v de Q tel que l’arc (u,v) existe faire
 //              d <- u.valeur + poids(u,v)
 //              Si d < v.valeur
@@ -66,9 +65,46 @@ public class Dijkstra {
 //              Fin Si
 //          Fin Pour
 //      Fin Tant que
+//                }
+//                return v;
+//      Fin
+//        }
+
+        public Valeur resoudre(Graphe g, String depart) {
+                //Valeur A = new Valeur();  //A un sommet (départ) de G
+                Valeur v = new Valeur();
+
+                ArrayList<String> Q = new ArrayList<String>();     // utilisation d’une liste de noeuds à traiter
+                //Q.add(depart);
+                for (String s : g.listeNoeuds()) {
+                        for (Arc a : g.suivants(s)) {
+                                v.setValeur(s,Double.MAX_VALUE);
+                                v.setParent(s,"indefini");
+                                Q.add(s);
+                        }
+                }
+                v.setValeur(depart,0);
+                while (!Q.isEmpty()) {
+                        String u=depart;
+                        int index=0;
+                                                for(int j=0;j<Q.size();j++) {
+                                                        if(v.getValeur(Q.get(j))>g.suivants(Q.get(j)).get(j).getCout()) {
+                                                                u=Q.get(j);
+                                                                index=j;
+                                                        }
+                                                }
+                        Q.remove(Q.get(index));
+                        for (int k=0; k<Q.size();k++) {
+                                if (g.suivants(Q.get(k)).get(k).getDest().equals(Q.get(k))) {
+                                        double d=v.getValeur(u)+g.suivants(Q.get(k)).get(k).getCout();
+                                        if (d<v.getValeur(Q.get(k))) {
+                                                v.setValeur(Q.get(k),d);
+                                                v.setParent(Q.get(k),u);
+                                        }
+                                }
+                        }
                 }
                 return v;
-//      Fin
         }
 
 }
---
+++ test/TestDijkstra.java
@@ -0,0 +1,140 @@
+import Algorithme.Dijkstra;
+import Graphes.GrapheListe;
+import org.junit.jupiter.api.Test ;
+import java.util.ArrayList;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDijkstra {
+    /**
+     * test avec un graphe construit correctement
+     */
+    @Test
+    public void testValide() {
+        ArrayList<String> noeuds = new ArrayList<>() ;
+        noeuds.add("A") ;
+        noeuds.add("B") ;
+        noeuds.add("C") ;
+        noeuds.add("D") ;
+        noeuds.add("E") ;
+        GrapheListe Gl = new GrapheListe(noeuds);
+
+        Gl.ajouterArc("A","B",12) ;
+        Gl.ajouterArc("A","D",87) ;
+        Gl.ajouterArc("B","E",11) ;
+        Gl.ajouterArc("C","A",19) ;
+        Gl.ajouterArc("D","B",23) ;
+        Gl.ajouterArc("D","C",10) ;
+        Gl.ajouterArc("E","D",43) ;
+
+        System.out.println(Gl);
+
+//        try {
+//            System.out.println(Gl.suivants("B"));
+//        } catch (NullPointerException e) {
+//            assertTrue(false);
+//        }
+
+//        assertEquals(5,noeuds.size());
+        Dijkstra d= new Dijkstra();
+        System.out.println(d.resoudre(Gl,"A"));
+        //assertEquals(,12);
+    }
+
+    /**
+     * test avec la recherche d'un arc inexistant
+     */
+//    @Test
+//    public void testArcInexistant() {
+//        ArrayList<String> noeuds = new ArrayList<>() ;
+//        noeuds.add("A") ;
+//        noeuds.add("B") ;
+//        noeuds.add("C") ;
+//        noeuds.add("D") ;
+//        noeuds.add("E") ;
+//        GrapheListe Gl = new GrapheListe(noeuds);
+//
+//        Gl.ajouterArc("A","B",12) ;
+//        Gl.ajouterArc("A","D",87) ;
+//        Gl.ajouterArc("B","E",11) ;
+//        Gl.ajouterArc("C","A",19) ;
+//        Gl.ajouterArc("D","B",23) ;
+//        Gl.ajouterArc("D","C",10) ;
+//        Gl.ajouterArc("E","D",43) ;
+//
+//        System.out.println(Gl);
+//
+//        try {
+//            System.out.println(Gl.suivants("B"));
+//        } catch (NullPointerException e) {
+//            assertTrue(false);
+//        }
+//
+//        assertEquals(5,noeuds.size());
+//        Exception IndexOutOfBoundsException = assertThrows(
+//                java.lang.IndexOutOfBoundsException.class,
+//                ()-> {
+//                    Gl.getAdjacence().get(Gl.getIndice("B")).getArcs().get(123).getDest();}
+//        );
+//    }
+
+    /**
+     * test avec des sommets sans adjacents
+     */
+//    @Test
+//    public void testValideAvecNoeudInexistant() {
+//        ArrayList<String> noeuds = new ArrayList<>() ;
+//        noeuds.add("A") ;
+//        noeuds.add("B") ;
+//        noeuds.add("C") ;
+//        noeuds.add("D") ;
+//        noeuds.add("E") ;
+//        GrapheListe Gl = new GrapheListe(noeuds);
+//
+//        Gl.ajouterArc("A","B",12) ;
+//        Gl.ajouterArc("A","D",87) ;
+//        Gl.ajouterArc("E","D",43) ;
+//
+//        System.out.println(Gl);
+//
+//        try {
+//            System.out.println(Gl.suivants("E"));
+//        } catch (NullPointerException e) {
+//            assertTrue(false);
+//        }
+//
+//        assertEquals(5,noeuds.size());
+//        assertEquals(Gl.getAdjacence().get(Gl.getIndice("E")).getArcs().get(0).getDest(),"D");
+//    }
+
+    /**
+     * test de avec un graphe construit Avec la liste de noeud Incomplète
+     */
+//    @Test
+//    public void testValideAvecListeNoeudIncomplete() {
+//        ArrayList<String> noeuds = new ArrayList<>() ;
+//        noeuds.add("A") ;
+//        noeuds.add("B") ;
+//        GrapheListe Gl = new GrapheListe(noeuds);
+//
+//        Gl.ajouterArc("A","B",12) ;
+//        Gl.ajouterArc("A","D",87) ;
+//        Gl.ajouterArc("B","E",11) ;
+//        Gl.ajouterArc("C","A",19) ;
+//        Gl.ajouterArc("D","B",23) ;
+//        Gl.ajouterArc("D","C",10) ;
+//        Gl.ajouterArc("E","D",43) ;
+//
+//        System.out.println(Gl);
+//
+//        try {
+//            System.out.println(Gl.suivants("D"));
+//        } catch (NullPointerException e) {
+//            assertTrue(false);
+//        }
+//
+//        assertEquals(5,noeuds.size());
+//        assertEquals(Gl.getAdjacence().get(Gl.getIndice("D")).getArcs().get(1).getDest(),"C");
+//    }
+}
