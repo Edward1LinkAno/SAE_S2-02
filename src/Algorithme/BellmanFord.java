0d140ed3338312c68cf51a08c4e9b80d0b003b35
Antonin-M <146951595+Antonin-M@users.noreply.github.com>
2024-05-31 16:52:12 +0200

    Algorithme BellmanFord + code java

--- src/Algorithme/BellmanFord.java
+++ src/Algorithme/BellmanFord.java
@@ -3,43 +3,78 @@ package Algorithme;
 import Graphes.Arc;
 import Graphes.Graphe ;
 import Graphes.Valeur ;
-import java.util.List;
+
 
 public class BellmanFord {
 
-    /**
-     * Cette méthode prend un graphe g et un String représentant le nœud de départ
-     * en paramètre et retourne un objet valeur correctement construit contenant les
-     * distances et les parents de chaque nœud (après convergence de l’algorithme).
-     */
-    public Valeur resoudre(Graphe g, String depart){
-        Valeur valeur = new Valeur() ;
-        List<String> noeud = g.listeNoeuds() ;
-        double val1 = 0 ;
+//        Algorithme :
+//        Entrées :
+//        G un graphe orienté avec une pondération des arcs (coût ou poids)
+//        A un sommet (départ) de G
 
-        for (String s : noeud) {
-            for (int i=0; i<g.suivants(s).size(); i++) {
-                valeur.setValeur(s,Double.MAX_VALUE);
-                valeur.setParent(s,"indefini");
-            }
+//        Début
+//        Si le noeud de départ n'est pas dans le graphe
+//              return null
+//        fin si
+
+//        Pour chaque sommet v de G faire
+//                v.valeur <- Infini
+//                v.parent <- Indéfini
+//        Fin Pour
+//        a.valeur <- 0
+
+//        boolean arret <- faux;
+//        Tant que non arret faire
+//            arret <- vrai
+//              Pour chaque sommet s de liste de noeud faire
+//                  Pour chaque arc a dans les successeurs
+//                      successeur <- la destination de a
+//                      minimum <- v.valeur(s) + le cout de a
+
+//                      Si minimum < successeur.valeur alors
+//                          v.valeur <- minimum
+//                          v.parent <- s
+//                          arret <- faux
+//                      fin si
+//                  fin pour
+//              fin pour
+//        fin tant
+
+
+//        retourne v
+//        fin
+
+    public Valeur resoudre(Graphe g, String depart) {
+        if (!g.listeNoeuds().contains(depart)) {
+            System.out.println("Le noeud de départ est inexistant");
+            return null;
         }
 
-        valeur.setParent(depart,null);
-        valeur.setValeur(depart,val1) ;
+        Valeur v = new Valeur();
 
-        for (String s : noeud){
-            double val2 = Double.MAX_VALUE ;
-            for (Arc a : g.suivants(s)) {
-                double val = a.getCout();
-                valeur.setParent(a.getDest(),s);
-                valeur.setValeur(a.getDest(),val+val1) ;
-                if (val+val1<val2){
-                    val2 = val+val1 ;
+        for (String s : g.listeNoeuds()) {
+            v.setValeur(s, Double.MAX_VALUE);
+            v.setParent(s, "indefini");
+        }
+        v.setValeur(depart, 0);
+
+        boolean arret = false;
+        while (!arret) {
+            arret=true;
+            for (String s : g.listeNoeuds()) {
+                for (Arc a : g.suivants(s)) {
+
+                    String voisin = a.getDest();
+                        double minimum = v.getValeur(s) + a.getCout();
+
+                        if (minimum < v.getValeur(voisin)) {
+                            v.setValeur(voisin, minimum);
+                            v.setParent(voisin, s);
+                            arret=false;
+                        }
+                    }
                 }
             }
-            val1 = val2 ;
-        }
-
-        return valeur ;
+        return v;
     }
 }
